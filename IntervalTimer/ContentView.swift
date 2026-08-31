cb98f8a3b0c7318effb361cf2ed2bc76e2b8cec1
Roger Brockett <rbrockett@college.harvard.edu>
2026-08-31 15:18:22 -0400

    Initial Commit

---
+++ IntervalTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IntervalTimer
+//
+//  Created by Roger Brockett on 8/31/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IntervalTimer/IntervalTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntervalTimerApp.swift
+//  IntervalTimer
+//
+//  Created by Roger Brockett on 8/31/26.
+//
+
+import SwiftUI
+
+@main
+struct IntervalTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
